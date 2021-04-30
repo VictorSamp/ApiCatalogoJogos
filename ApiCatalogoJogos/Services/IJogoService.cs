e22942fcf110a635cfea079c1bdc0bd89e945e6e
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-30 09:01:16 -0300

    feat: cria interface e classe Service de jogos

---
+++ ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,23 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> ObterTodosJogos(int pagina, int quantidade);
+
+        Task<JogoViewModel> ObterJogoPorId(Guid id);
+
+        Task<JogoViewModel> InserirJogo(JogoInputModel jogo);
+
+        Task Atualizar(Guid id, JogoInputModel jogo);
+
+        Task Atualizar(Guid id, double preco);
+
+        Task Remover(Guid id);
+    }
+}

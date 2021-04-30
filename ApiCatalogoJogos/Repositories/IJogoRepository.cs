4c81bda976c073402ff054dcaa051c8fe4bf7bab
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-30 07:31:56 -0300

    feat: cria interface de repositorio IJogoRepository

---
+++ ApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,22 @@
+﻿using ApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> ObterTodosJogos(int pagina, int quantidade);
+
+        Task<Jogo> ObterJogoPorId(Guid id);
+
+        Task<List<Jogo>> Obter(string nome, string produtora);
+
+        Task Inserir(Jogo jogo);
+
+        Task Atualizar(Jogo jogo);
+
+        Task Remover(Guid id);
+    }
+}

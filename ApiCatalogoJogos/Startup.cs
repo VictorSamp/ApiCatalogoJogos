fbde6695111c18e890f47cc8123a01853b218f50
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-30 09:36:03 -0300

    feat: adiciona AddScoped na classe Startup

--- ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/Startup.cs
@@ -1,3 +1,6 @@
+using ApiCatalogoJogos.Repositories;
+using ApiCatalogoJogos.Services;
+using ExemploApiCatalogoJogos.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,8 @@ namespace ApiCatalogoJogos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
